706aad4b8dd62193ea8c89b8bbbf4a86e1c3a830
skjha336-dotcom <skjha336@gmail.com>
2026-01-08 17:14:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,8 +98,8 @@ resource "aws_security_group" "web_sg" {
 # EC2 Instance
 ##################
 resource "aws_instance" "web" {
-  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
-  instance_type          = "t2.micro"
+  ami                    = "ami = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  instance_type          = "t3.micro"
   subnet_id              = "${aws_subnet.public.id}"
   vpc_security_group_ids = ["${aws_security_group.web_sg.id}"]
   key_name               = "my-keypair"
