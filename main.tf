c4b7efdce2f88d8966d1a3c72b11258b2c360a32
skjha336-dotcom <skjha336@gmail.com>
2026-01-08 16:47:14 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,128 @@
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.0"
+}
+
+##################
+# VPC
+##################
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags {
+    Name = "demo-vpc"
+  }
+}
+
+##################
+# Internet Gateway
+##################
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags {
+    Name = "demo-igw"
+  }
+}
+
+##################
+# Public Subnet
+##################
+resource "aws_subnet" "public" {
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags {
+    Name = "public-subnet"
+  }
+}
+
+##################
+# Route Table
+##################
+resource "aws_route_table" "public_rt" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+
+  tags {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = "${aws_subnet.public.id}"
+  route_table_id = "${aws_route_table.public_rt.id}"
+}
+
+##################
+# Security Group
+##################
+resource "aws_security_group" "web_sg" {
+  name   = "web-sg"
+  vpc_id = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["YOUR_IP/32"]   # 🔐 change this
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "web-sg"
+  }
+}
+
+##################
+# EC2 Instance
+##################
+resource "aws_instance" "web" {
+  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  instance_type          = "t2.micro"
+  subnet_id              = "${aws_subnet.public.id}"
+  vpc_security_group_ids = ["${aws_security_group.web_sg.id}"]
+  key_name               = "my-keypair"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Hello from Terraform EC2</h1>" > /var/www/html/index.html
+              EOF
+
+  tags {
+    Name = "web-ec2"
+  }
+}
+
+##################
+# Route 53 Record
+##################
+resource "aws_route53_record" "web_dns" {
+  zone_id = "ZXXXXXXXXXXXX"   # hosted zone ID
+  name    = "www.example.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["${aws_instance.web.public_ip}"]
+}
